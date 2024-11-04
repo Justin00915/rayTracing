8eda74ab1cdd168c7c0318a97a7bb5d7e55c6455
justin <walter1justin1@gmail.com>
2024-11-04 21:33:44 +0100

    Adjusted code style

--- Vector.h
+++ Vector.h
@@ -1,7 +1,8 @@
 #pragma once
 #include<math.h>
 
-class Vector {
+class Vector
+{
 public:
 	double x;
 	double y;
@@ -10,116 +11,141 @@ public:
 	Vector(double x = 0, double y = 0, double z = 0)
 		: x(x), y(y), z(z) { }
 
-	double GetLength() const {
+	double GetLength() const
+	{
 		return sqrt(x * x + y * y + z * z);
 	}
-	Vector GetNormalized() {
+	Vector GetNormalized()
+	{
 		double l = GetLength();
-		if (l == 0) {
+		if (l == 0)
+		{
 			return *this;
 		}
 		return Vector(x / l, y / l, z / l);
 	}
 
-	void Normalize() {
+	void Normalize()
+	{
 		double l = GetLength();
 		x /= l;
 		y /= l;
 		z /= l;
 	}
 
-	Vector GetAbsolutized() {
+	Vector GetAbsolutized()
+	{
 		return Vector(fabs(x), fabs(y), fabs(z));
 	}
 
-	void Absolutize() {
+	void Absolutize()
+	{
 		x = fabs(x);
 		y = fabs(y);
 		z = fabs(z);
 	}
 
-	double Dot(Vector other) const {
+	double Dot(Vector other) const
+	{
 		return x * other.x + y * other.y + z * other.z;
 	}
 
-	double GetDist(Vector other) {
+	double GetDist(Vector other)
+	{
 		return (*this - other).GetLength();
 	}
 
-	double GetMax() const {
+	double GetMax() const
+	{
 		return fmax(fmax(x, y), z);
 	}
 
-	double GetAbsMax() const {
+	double GetAbsMax() const
+	{
 		return fmax(fmax(fabs(x), fabs(y)), fabs(z));
 	}
 
-	double GetMin() const {
+	double GetMin() const
+	{
 		return fmin(fmin(x, y), z);
 	}
 
-	double GetAbsMin() const {
+	double GetAbsMin() const
+	{
 		return fmin(fmin(fabs(x), fabs(y)), fabs(z));
 	}
 
 	//Component-wise
-	Vector operator+(Vector other) {
+	Vector operator+(Vector other)
+	{
 		return Vector(x + other.x, y + other.y, z + other.z);
 	}
 
 	//Component-wise
-	Vector operator+=(Vector other) {
+	Vector operator+=(Vector other)
+	{
 		return Vector(x + other.x, y + other.y, z + other.z);
 	}
 
 	//Component-wise
-	Vector operator-(Vector other) {
+	Vector operator-(Vector other)
+	{
 		return Vector(x - other.x, y - other.y, z - other.z);
 	}
 
 	//Component-wise
-	Vector operator-=(Vector other) {
+	Vector operator-=(Vector other)
+	{
 		return Vector(x - other.x, y - other.y, z - other.z);
 	}
 
 	//Component-wise
-	Vector operator*(Vector other) {
+	Vector operator*(Vector other)
+	{
 		return Vector(x * other.x, y * other.y, z * other.z);
 	}
 
 	//Component-wise
-	Vector operator*=(Vector other) {
+	Vector operator*=(Vector other)
+	{
 		return Vector(x * other.x, y * other.y, z * other.z);
 	}
 
 	//Component-wise
-	Vector operator/(Vector other) {
+	Vector operator/(Vector other)
+	{
 		return Vector(x / other.x, y / other.y, z / other.z);
 	}
 
 	//Component-wise
-	Vector operator/=(Vector other) {
+	Vector operator/=(Vector other)
+	{
 		return Vector(x / other.x, y / other.y, z / other.z);
 	}
 
-	Vector operator*(double lambda) {
+	Vector operator*(double lambda)
+	{
 		return Vector(x * lambda, y * lambda, z * lambda);
 	}
 
-	Vector operator*=(double lambda) {
+	Vector operator*=(double lambda)
+	{
 		return Vector(x * lambda, y * lambda, z * lambda);
 	}
 
-	Vector operator/(double lambda) {
+	Vector operator/(double lambda)
+	{
 		return Vector(x / lambda, y / lambda, z / lambda);
 	}
 
-	Vector operator/=(double lambda) {
+	Vector operator/=(double lambda)
+	{
 		return Vector(x / lambda, y / lambda, z / lambda);
 	}
 
 	//Component-wise
-	bool operator==(Vector other) {
+	bool operator==(Vector other)
+	{
 		return x == other.x && y == other.y && z == other.z;
 	}
 };
